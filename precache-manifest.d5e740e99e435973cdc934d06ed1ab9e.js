6b7727f28b0b6c0219dcbdbff27e8dc60df26a8f
Vasya <v.onufriy@gmail.com>
2018-12-23 00:08:32 +0200

    Updates

---
+++ precache-manifest.d5e740e99e435973cdc934d06ed1ab9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "635860fd533afff61498",
+    "url": "/contact_manager/static/js/runtime~main.635860fd.js"
+  },
+  {
+    "revision": "07909fd3f2d2af8ef354",
+    "url": "/contact_manager/static/js/main.07909fd3.chunk.js"
+  },
+  {
+    "revision": "1f607283ae52d1036192",
+    "url": "/contact_manager/static/js/1.1f607283.chunk.js"
+  },
+  {
+    "revision": "07909fd3f2d2af8ef354",
+    "url": "/contact_manager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "1f607283ae52d1036192",
+    "url": "/contact_manager/static/css/1.31338c6c.chunk.css"
+  },
+  {
+    "revision": "2d2ff05aee58d07c530c26130116a8d4",
+    "url": "/contact_manager/index.html"
+  }
+];
